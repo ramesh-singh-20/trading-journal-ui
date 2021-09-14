a633e68dd238ac10fa7798c5dd688cb9d1c8ceb4
Ramesh Singh <singh.ramesh.eee@gmail.com>
2021-09-14 12:18:57 -0400

    Added code for trade type by year and chart type.

--- src/app/components/analytics-by-year-and-chart-pattern/analytics-by-year-and-chart-pattern.component.ts
+++ src/app/components/analytics-by-year-and-chart-pattern/analytics-by-year-and-chart-pattern.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Analytics } from 'src/app/model/analytics.interface';
+import { AnalyticsByYearAndChartTypeService } from 'src/app/service/analytics-by-year-and-chart-type.service';
 
 @Component({
   selector: 'app-analytics-by-year-and-chart-pattern',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnalyticsByYearAndChartPatternComponent implements OnInit {
 
-  constructor() { }
+  constructor(private analyticsByYearAndChartPattern: AnalyticsByYearAndChartTypeService) { }
 
   ngOnInit(): void {
+    this.getChartPatternValues();
+  }
+  
+
+
+  hasResults: boolean= false;
+
+  chartPatternValues:String[]= [];
+
+
+  displayedColumns: string[] = ['Year', 'Total Trades', 'Wins', 'Losses', 'Batting Average', 'Average Gain', 'Average Loss', 
+                                'Win Loss Ratio', 'Largest Gain', 'Largest Loss', 'Net Average',
+                                'Percent Win', 'Percent Loss', 'Adjusted Win Loss Ratio',
+                                'Avg Days Gains Held', 'Avg Days Losses Held'];
+                                
+  analyticsByYearAndChartPatternDataSource :Analytics[]= [];
+
+
+  getAllAnalyticsByYearAndChartPattern(chartPattern: string): void {
+    this.analyticsByYearAndChartPattern.getAllAnalyticsByYearAndChartPattern(chartPattern)
+        .subscribe(
+          response => {
+            if(response.data!== null){
+              this.hasResults= true;
+             this.analyticsByYearAndChartPatternDataSource= response.data;
+            }else{
+              console.log("Error.");
+            }
+          },
+          error =>{
+            console.log("Error: "+error);
+          }
+        );
+  }
+
+  getChartPatternValues(){
+    this.analyticsByYearAndChartPattern.getChartPatternValues()
+         .subscribe(
+           response => {
+             this.chartPatternValues= response;
+           }
+         )
   }
 
 }
--- src/app/service/analytics-by-year-and-chart-type.service.ts
+++ src/app/service/analytics-by-year-and-chart-type.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AnalyticsResponse } from '../model/analytics-response.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnalyticsByYearAndChartTypeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private analyticsByYearAndChartPatternURL = 'http://localhost:8080/analytics/chart-pattern/year/';
+  private distinctChartPatternURL= 'http://localhost:8080/analytics/distinct/chart-pattern';
+
+
+  getAllAnalyticsByYearAndChartPattern(chartPattern: string): Observable<AnalyticsResponse> {
+    let headers = new HttpHeaders()
+      .set('Access-Control-Allow-Origin', '*')
+      .set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
+      .set('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+      let url: string= this.analyticsByYearAndChartPatternURL+chartPattern;
+    return this.http.get<AnalyticsResponse>(url, {
+      headers
+    });
+  }
+
+  getChartPatternValues(): Observable<String[]> {
+    let headers = new HttpHeaders()
+      .set('Access-Control-Allow-Origin', '*')
+      .set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
+      .set('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+    return this.http.get<String[]>(this.distinctChartPatternURL, {
+      headers
+    });
+  }
 }
