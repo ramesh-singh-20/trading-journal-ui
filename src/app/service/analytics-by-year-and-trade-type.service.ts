8cc7d7df94b0641924d9dbe92a1213b2d8b68b65
Ramesh Singh <singh.ramesh.eee@gmail.com>
2021-09-15 22:44:22 -0400

    Added service method to get trade by month and trade type.

--- src/app/service/analytics-by-year-and-trade-type.service.ts
+++ src/app/service/analytics-by-year-and-trade-type.service.ts
@@ -12,6 +12,7 @@ export class AnalyticsByYearAndTradeTypeService {
 
   private analyticsByYearAndTradeTypeURL = 'http://localhost:8080/analytics/trade-type/year/';
   private distinctTradeTypeURL= 'http://localhost:8080/analytics/distinct/trade-type';
+  private analyticsByMonthAndTradeTypeURL = 'http://localhost:8080/analytics/trade-type/';
 
 
   getAllAnalyticsByYearAndTradeType(tradeType: string): Observable<AnalyticsResponse> {
@@ -31,6 +32,17 @@ export class AnalyticsByYearAndTradeTypeService {
       .set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
       .set('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
     return this.http.get<String[]>(this.distinctTradeTypeURL, {
+      headers
+    });
+  }
+
+  getAllAnalyticsByMonthAndTradeType(tradeType: string, year: string): Observable<AnalyticsResponse> {
+    let headers = new HttpHeaders()
+      .set('Access-Control-Allow-Origin', '*')
+      .set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
+      .set('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+      let url: string= this.analyticsByYearAndTradeTypeURL+tradeType+'/'+year;
+    return this.http.get<AnalyticsResponse>(url, {
       headers
     });
   }
