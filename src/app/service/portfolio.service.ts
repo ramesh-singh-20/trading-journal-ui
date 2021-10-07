a6727439e04b114a1ac415e1860e47b0c092b858
Ramesh Singh <singh.ramesh.eee@gmail.com>
2021-10-07 14:24:44 -0400

    Added service class for Portfolio.

--- src/app/service/portfolio.service.ts
+++ src/app/service/portfolio.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PortfolioResponse } from '../model/portfolio-response.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +11,19 @@ export class PortfolioService {
   constructor(private http: HttpClient) { }
 
   private portfolioURL = 'http://localhost:8080/portfolio';
+
+  getHeaders(): HttpHeaders {
+    return new HttpHeaders()
+      .set('Access-Control-Allow-Origin', '*')
+      .set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
+      .set('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+  }
+
+  getPortfolioDetails(): Observable<PortfolioResponse> {
+      
+    return this.http.get<PortfolioResponse>(this.portfolioURL, {
+      headers: this.getHeaders()
+    });
+  }
 
 }
